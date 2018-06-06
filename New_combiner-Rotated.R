a5b64b1d8b6cf3c902f843bfca41461fd8a6f5ef
iljonas <iljonas6@gmail.com>
2018-06-05 23:54:35 -0500

    created rotated source and R; updated R script's ability to read source

--- New_combiner-Rotated.R
+++ New_combiner-Rotated.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(tidyr)
 library(rvest)
 
+trim <- function(t){gsub("^\\s+|\\s+$", "", t)}
+
 setMeta.study <- function(source){
      temp.meta <- read.table(source, sep = "\t", 
                              comment.char = "", fill = TRUE, stringsAsFactors = FALSE)
@@ -12,12 +14,10 @@ setMeta.study <- function(source){
      return(temp.meta)
 }
 
-setMeta.samples <- function(source, m.study){
-     temp.meta <- read.csv(source, comment.char = "#", sep = '\t', na.strings = 'NA', stringsAsFactors = FALSE)
-     row.names(temp.meta) <- subset(m.study, Field == "Col_Meaning", Description, drop = TRUE) %>%
-          strsplit(",") %>%
-          unlist
-     return(temp.meta)
+setMeta.samples <- function(source){
+     temp.meta <- read.table(source, comment.char = "#", sep = '\t', na.strings = 'NA', stringsAsFactors = FALSE)
+     row.names(temp.meta) <- temp.meta$V1
+     return(temp.meta[-1])
 }
 
 setSeries <- function(seriesID, m.samples){
@@ -26,26 +26,45 @@ setSeries <- function(seriesID, m.samples){
      local.path <- paste0(series.folder ,"\\" , seriesID, ".txt.gz")
      
      if(!file.exists(local.path)){
-          ftp.path <- paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series", 
-                            gsub("...$", "nnn", seriesID), seriesID, "matrix",
-                            paste0(seriesID, "_series_matrix.txt.gz"), 
-                            sep = "/")
+          if(!(grepl("^ftp:\\\\", seriesID))) {
+               ftp.path <- paste("ftp://ftp.ncbi.nlm.nih.gov/geo/series", 
+                                 gsub("...$", "nnn", seriesID), seriesID, "matrix",
+                                 paste0(seriesID, "_series_matrix.txt.gz"), 
+                                 sep = "/")
+          } else {
+               ftp.path = seriesID
+          }
           download.file(ftp.path, destfile = local.path)
      }
      
-     temp.series <- read.csv(gzfile(local.path), comment.char = "!", sep = '\t', na.strings = c("", "null"), stringsAsFactors = FALSE)
-     colnames(temp.series)[-1] <- sapply(m.samples,  
-                                         FUN = function(x){paste(x[!is.na(x)], collapse = "|")})
-     colnames(temp.series) <- gsub(" ", "_", colnames(temp.series))
+     temp.series <- read.csv(gzfile(local.path), comment.char = "!", sep = '\t', 
+                             na.strings = c("", "null"), stringsAsFactors = FALSE)
+     temp.series <- temp.series[, order(names(temp.series))]
+     temp.series <- select(temp.series, ID_REF, everything())
+     m.samples <- m.samples[order(row.names(m.samples)), ]
+     if(!(FALSE %in%
+          (sapply(names(temp.series)[-1], function(x){trim(x)}) 
+               == sapply(row.names(m.samples), function(x){trim(x)})))) {
+          names(temp.series)[-1] <- apply(m.samples, 1, FUN = function(x){paste(trim(x), collapse = "|")})
+          names(temp.series) <- gsub(" ", "_", names(temp.series))
+     }
+     else{
+          stop("The samples names in the form don't match the sample names in the series data.
+               Please check over the entries and rerun the program.")
+     }
      return(temp.series)
 }
 
 setPlatform <- function(platformID){
-     pURL <- paste0("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?view=data&acc=", platformID)
-     pText <- read_html(pURL) %>%
-          html_nodes("pre") %>%
-          html_text
-     read.csv(text = pText, comment.char = "#", sep = "\t", quote = "", na.strings = "")
+     if(grepl("[^.:\\\\]", platformID)) {
+          pURL <- paste0("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?view=data&acc=", platformID)
+          pText <- read_html(pURL) %>%
+               html_nodes("pre") %>%
+               html_text
+          read.csv(text = pText, comment.char = "#", sep = "\t", quote = "", na.strings = "", stringsAsFactors = FALSE)
+     } else {
+          read.csv(platformID, comment.char = "#", sep = "\t", quote = "", na.strings = "", stringsAsFactors = FALSE)
+     }
 }
 
 newControls <- function(c.source, m){
@@ -69,20 +88,27 @@ findMatches <- function(dfname, df){
      }
 }
 
-combine.groups <- function(g){
+combine.groups <- function(g, m.study){
      combined.group <- select(g, Expression.ID)
      all.group <- as.data.frame(c(Expression.ID = combined.group, select(g, contains("160_min"))))
      control.group <- select(g, contains("120_min"))
      test.group <- select(g, contains("|60_min"))
+     diff.calc <- subset(meta.study, Field == "Diff_Calc", Description, drop = TRUE)
+     comp.calc <- subset(meta.study, Field == "Comparison_Calc", Description, drop = TRUE)
      counter <- ncol(all.group)
      for(t in test.group){
           for(c in control.group){
                counter <- counter + 1
-               all.group[, counter] <- t - c
+               all.group[, counter] <- if_else(diff.calc == "Subtract", t - c, 
+                                               if_else(diff.calc == "Log2", log(t / c, 2), 
+                                               stop("Error in difference calculation; check JavaScript")))
           }
      }
      
-     all.combined <- apply(all.group[, -1], 1, FUN = mean, na.rm = TRUE)
+     all.combined <- apply(all.group[, -1], 1, if_else(comp.calc == "Mean", FUN = mean,
+                                                      if_else(comp.calc == "Sum", FUN = sum,
+                                                              stop("Error in combination calculation; check JavaScript"))) 
+                           , na.rm = TRUE)
      combined.group$placeholder <- all.combined
      #new.name <- unique(gsub("^[^|]*\\|", "", names(g)[-1]))
      names(combined.group)[2] <- unique(gsub("\\|Dapt.*", "", names(g)[-1]))
@@ -90,10 +116,10 @@ combine.groups <- function(g){
      return(all.combined)
 }
 
-meta.source <- "C:\\Users\\Isaac\\Documents\\Capstone Files\\GEO-Antimicrobial-Adjunct-Project\\Temp_Source.txt"
+meta.source <- "C:\\Users\\Isaac\\Documents\\Capstone Files\\GEO-Antimicrobial-Adjunct-Project\\Temp_Source-Rotated.txt"
 
 meta.study <- setMeta.study(meta.source)
-meta.samples <- setMeta.samples(meta.source, meta.study)
+meta.samples <- setMeta.samples(meta.source)
 
 series.id <- subset(meta.study, Field == "Study", Description, drop = TRUE)
 series <- setSeries(series.id, meta.samples)
@@ -124,4 +150,4 @@ if(outside.controls != ""){
 
 sep.master<- sapply(colnames(master), findMatches, df = master)
 
-new.master <- sapply(sep.master, FUN = combine.groups)
+new.master <- sapply(sep.master, FUN = combine.groups, m.study = meta.study)
